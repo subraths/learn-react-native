762268050373541a601fba0e8b265046a15b97e0
Subrath <subrathitachi6@gmail.com>
2025-11-22 13:45:17 +0530

    add test route

---
+++ app/jee/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from "@/components/ui/text";
+
+export default function Jee() {
+  return <Text>Hello im jee</Text>;
+}
---
+++ app/jee/kee.tsx
@@ -0,0 +1,5 @@
+import { Text } from "@/components/ui/text";
+
+export default function Kee() {
+  return <Text>Hello im kee</Text>;
+}
